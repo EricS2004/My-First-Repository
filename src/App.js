dd4559f2fec8523de0d93eecd4f4997c61b18c91
EricS2004 <57140176+EricS2004@users.noreply.github.com>
2021-10-18 17:07:41 -0400

    2021 and HS: WCHS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Buenos Tardes</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>2021 HighSchool: WCHS</h2>
     </div>
   );
 }
