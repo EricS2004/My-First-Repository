23575aa77a322cb8726e550518afd666fc3ac0b9
EricS2004 <57140176+EricS2004@users.noreply.github.com>
2021-10-18 17:59:59 -0400

    Buenos Tardes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Buenos Tardes</h1>
-      <h2>2021 HighSchool: WCHS</h2>
-    </div>
-  );
+  if (false) {
+    return (
+      <div className="App">
+        <h1>Buenos Tardes</h1>
+        <h2>2021 HighSchool: WCHS</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Buenos Tardes 2</h1>
+        <h2>2021 HighSchool: WCHS</h2>
+        <img src="https://www.looper.com/img/gallery/black-clover-season-5-release-date-cast-and-plot-what-we-know-so-far/l-intro-1621044275.jpg" />
+      </div>
+    );
+  }
 }
